a12cb5da771ddae903e73fceff71138ba414eb2b
ElyasPicpay <elyas.santana@picpay.com>
2023-03-09 10:32:03 -0300

    fix: ajuste logs watson

--- logs.py
+++ logs.py
@@ -1,48 +1,32 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-import requests
+from datetime import datetime
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2023, 3, 1),
-    "email": ["airflow@example.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
-
-dag = DAG(
-    "send_watson_logs_to_airflow",
-    default_args=default_args,
-    schedule_interval=timedelta(minutes=3),
-)
+from airflow.operators.python import PythonOperator
 
 
 def send_logs():
     # Coloque aqui suas credenciais do Watson Assistant V2 e o ID da sua workspace
-    watson_apikey = "SUA_APIKEY_DO_WATSON_ASSISTANT_V2"
-    watson_url = "URL_DO_WATSON_ASSISTANT_V2"
-    workspace_id = "ID_DA_SUA_WORKSPACE_DO_WATSON_ASSISTANT_V2"
+    # watson_apikey = "SUA_APIKEY_DO_WATSON_ASSISTANT_V2"
+    # watson_url = "URL_DO_WATSON_ASSISTANT_V2"
+    # workspace_id = "ID_DA_SUA_WORKSPACE_DO_WATSON_ASSISTANT_V2"
 
     # Definindo as datas para consulta dos logs do Watson Assistant
-    now = datetime.now()
-    end_time = now.isoformat() + "Z"  # Data e hora atual
-    start_time = (now - timedelta(hours=1)).isoformat() + "Z"  # Há 1 hora atrás
+    # now = datetime.now()
+    # end_time = now.isoformat() + "Z"  # Data e hora atual
+    # start_time = (now - timedelta(hours=1)).isoformat() + "Z"  # Há 1 hora atrás
 
     # Definindo o endpoint da API do Watson Assistant para consulta de logs
-    url = watson_url + "/v2/assistants/" + workspace_id + "/logs"
+    # url = watson_url + "/v2/assistants/" + workspace_id + "/logs"
+    url = "https://escola-api.caprover.programadornoob.io/start_point/health"
 
     # Definindo os headers para a requisição HTTP
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + watson_apikey,
-    }
+    # headers = {
+    #     "Content-Type": "application/json",
+    #     "Authorization": "Bearer " + watson_apikey,
+    # }
 
     # Definindo os parâmetros da requisição HTTP
-    params = {"start_time": start_time, "end_time": end_time}
+    # params = {"start_time": start_time, "end_time": end_time}
 
     # Fazendo a requisição HTTP para o Watson Assistant
     response = requests.get(url, headers=headers, params=params)
@@ -69,10 +53,15 @@ def send_logs():
         logging.info(log)
 
 
-send_logs_task = PythonOperator(
-    task_id="send_watson_logs",
-    python_callable=send_logs,
-    dag=dag,
-)
+with DAG(
+    "send_watson_logs_to_airflow",
+    start_date=datetime(2023, 3, 8),
+    schedule_interval=timedelta(minutes=3),
+    catchup=False,
+) as dag:
 
-send_logs_task
+    send_logs_watson = PythonOperator(
+        task_id="Logs Watson", python_callable=send_logs
+    )
+
+    send_logs_watson
